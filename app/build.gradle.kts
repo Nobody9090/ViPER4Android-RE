827c08c21045a6d8a70a3419f0a70e8160a8e8af
APOPHIS9283 <142638952+Nobody9090@users.noreply.github.com>
2025-08-30 18:55:22 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     id("kotlin-kapt")
     id("androidx.room")
+    alias(libs.plugins.android.x.room)
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.ksp)
 }
